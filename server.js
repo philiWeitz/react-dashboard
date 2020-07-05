f52f2e3e432353d9c240292d46ee10c2d18800fb
Philipp Weitz <philipp.weitz@futurice.com>
2020-07-05 10:38:44 +0300

    add production server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const buildDir = path.join(__dirname, '/build');
+
+const app = express();
+app.use(express.static(buildDir));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(buildDir, 'index.html'));
+});
+
+app.listen(port);
+console.log(`Server Started on port ${port}`);
